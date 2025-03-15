4d605eefc5c1ef5e67173f7dba39baef27d44e2a
Xingeria <nguyenthaitrieu2302@gmail.com>
2025-03-15 11:38:06 +0700

    Update main page

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const express = require('express')
-const app = express()
-const port = 3000
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.status(200).set('Content-type', 'text/html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, (error) => {
+  if (error) {
+    console.log(`Something went wrong!`, error);
+  } else {
+    console.log(`Example app listening on port ${port}`);
+  }
 })
+  
 
